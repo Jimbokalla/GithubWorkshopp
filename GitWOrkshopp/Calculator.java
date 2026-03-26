1eec9bfc66be33deddd9436ae3eb4e14976a7da5
leopers2004-a11y <leopers2004@gmail.com>
2026-03-26 09:59:00 +0100

    Update Calculator.java
    Add calclulatror, mul(a,b)

--- GitWOrkshopp/Calculator.java
+++ GitWOrkshopp/Calculator.java
@@ -1,4 +1,6 @@
 public class Calculator {
     public static double add( double a, double b) { return a + b;}
     public static double sub(double a, double b) {return a - b;}
+    public static double mul(doublr a, double b ) { return a * b; }
+    
 }
